9172501948cbe035808277f48ad665c38fcd16db
Eric Krinick <erickrinick@gmail.com>
2019-12-26 18:06:24 -0500

    finished for real this time

--- trainsched.js
+++ trainsched.js
@@ -10,37 +10,30 @@
 
   firebase.initializeApp(firebaseConfig);
   var trainInfo = firebase.database() 
-  var tempName = "NJ Transit"
-  var tempDestination = "New York Penn Station"
-  var tempFirstTrain = "4:00" 
-  var tempFrequency = "90"
+
   var trains = {
-    name: tempName, 
-    destination: tempDestination,
-    firstTrain: tempFirstTrain,
-    frequency: tempFrequency,    
   };
   
-  trainInfo.ref().push(trains)
   trainInfo.ref().on("child_added", function (childSnapshot){
-      //console.log(childSnapshot.val());
-      var thisTrain = childSnapshot.val();
-      var tFrequency = thisTrain.frequency;
-        console.log(thisTrain.firstTrain);
-        var firstTime = thisTrain.firstTrain;
-        var theTrainName = thisTrain.name;
-        var finalDestin = thisTrain.destination;
-
+    console.log(childSnapshot.val())
+    //console.log(childSnapshot.val());
+    var thisTrain = childSnapshot.val();
+    var tFrequency = thisTrain.frequency;
+    console.log(thisTrain.firstTrain);
+    var firstTime = thisTrain.firstTrain;
+    var theTrainName = thisTrain.name;
+    var finalDestin = thisTrain.destination;
+    
     var firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "years");
- 
+    
     var currentTime = moment();
-
+    
     // Difference between the times
     var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
-
+    
     // Time apart (remainder)
     var tRemainder = diffTime % tFrequency;
-
+    
     // Minute Until Train
     var tMinutesTillTrain = tFrequency - tRemainder;
     console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
@@ -48,4 +41,23 @@
     console.log("The final destination of this train is " + finalDestin);
     var nextTrain = moment().add(tMinutesTillTrain, "minutes");
     console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
+    $(".table > tbody").append(
+      $("<tr>").append(
+        $("<td>").text(theTrainName),
+        $("<td>").text(finalDestin),
+        $("<td>").text(tFrequency),
+        $("<td>").text(tMinutesTillTrain),
+        $("<td>").text(nextTrain)
+      )
+    );
   }) 
+  
+  $("#add-user").click(function(event){
+    event.preventDefault()
+    trains.name = $("#trainName").val()
+    trains.destination = $("#destination").val()
+    trains.firstTrain = $("#firstTrain").val()
+    trains.frequency = $("#frequency").val()
+    console.log(trains)
+    trainInfo.ref().push(trains)
+  });
